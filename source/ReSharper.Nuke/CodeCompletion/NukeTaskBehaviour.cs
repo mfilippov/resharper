f0f4c6d9aab9e0c5f113b76ada06d898c0d52437
Sebastian Karasek <sebastian@karasek.io>
2018-04-28 14:21:30 +0200

    Add code completion for tasks.

---
+++ source/ReSharper.Nuke/CodeCompletion/NukeTaskBehaviour.cs
@@ -0,0 +1,19 @@
+﻿// Copyright Sebastian Karasek, Matthias Koch 2018.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/ide-extensions/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.AspectLookupItems.Info;
+using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp.AspectLookupItems;
+
+namespace ReSharper.Nuke.CodeCompletion
+{
+    public class NukeTasksBehaviour : CSharpMethodsBehavior<MethodsInfo>
+    {
+        public NukeTasksBehaviour(MethodsInfo info)
+            : base(info)
+        {
+        }
+    }
+}
