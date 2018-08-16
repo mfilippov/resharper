62d26361e239d9356e5175036b1a797b6ef779dd
Mikhail Filippov <mikhail@filippov.me>
2018-08-16 14:40:52 +0300

    Rewrite run configuration creation logic.

--- source/rider/src/main/kotlin/com/jetbrains/rider/plugins/nuke/RunConfigurationManager.kt
+++ source/rider/src/main/kotlin/com/jetbrains/rider/plugins/nuke/RunConfigurationManager.kt
@@ -1,12 +1,11 @@
 package com.jetbrains.rider.plugins.nuke
 
-import com.intellij.execution.DefaultExecutionTarget
-import com.intellij.execution.ExecutionManager
-import com.intellij.execution.RunManager
+import com.intellij.execution.*
 import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
+import com.jetbrains.rider.model.BuildInvocation
 import com.jetbrains.rider.model.rdNukeModel
 import com.jetbrains.rider.model.runnableProjectsModel
 import com.jetbrains.rider.projectView.solution
@@ -14,41 +13,57 @@ import com.jetbrains.rider.run.configurations.project.DotNetProjectConfiguration
 import com.jetbrains.rider.run.configurations.project.DotNetProjectConfigurationType
 import com.jetbrains.rider.util.idea.LifetimedProjectComponent
 
-class RunConfigurationManager(project: Project, runManager: RunManager) : LifetimedProjectComponent(project) {
+class RunConfigurationManager(project: Project, private val runManager: RunManager) : LifetimedProjectComponent(project) {
     init {
-        project.solution.rdNukeModel.build.advise(componentLifetime) { it ->
-            var configuration = runManager.findConfigurationByName(it.target)
-            if (configuration == null) {
-                val configurationType = runManager.configurationFactories.single { it -> it is DotNetProjectConfigurationType }
-                val configurationFactory = configurationType.configurationFactories.first()
-                configuration = runManager.createRunConfiguration(it.target, configurationFactory)
-                configuration.isTemporary = true
-
-                val buildProjectFile = FileUtil.toSystemIndependentName(it.projectFile)
-                var dotnetProject = project.solution.runnableProjectsModel.projects.valueOrNull!!
-                        .single { it -> it.projectFilePath == buildProjectFile }
-
-                val dotnetConfiguration = configuration.configuration as DotNetProjectConfiguration
-                dotnetConfiguration.parameters.projectFilePath = dotnetProject.projectFilePath
-                dotnetConfiguration.parameters.projectKind = dotnetProject.kind
-                dotnetConfiguration.parameters.programParameters = it.target
-                if (it.skipDependencies)
-                    dotnetConfiguration.parameters.programParameters += " -skip"
-
-                configuration!!.checkSettings()
-
-                runManager.addConfiguration(configuration)
-            }
-
+        project.solution.rdNukeModel.build.advise(componentLifetime) { buildInvocation ->
+            val configuration = runManager.findConfigurationByName(getConfigurationName(buildInvocation))
+                    ?: createConfigurationAndSettings(buildInvocation)
             runManager.selectedConfiguration = configuration
-            val executionManager = ExecutionManager.getInstance(project)
-            val executor = if (it.debugMode) DefaultDebugExecutor.getDebugExecutorInstance() else DefaultRunExecutor.getRunExecutorInstance()
-            executionManager.restartRunProfile(
-                    project,
-                    executor,
-                    DefaultExecutionTarget.INSTANCE,
-                    configuration,
-                    null)
+            ExecutionManager.getInstance(project)
+                    .restartRunProfile(
+                            project,
+                            getExecutor(buildInvocation),
+                            DefaultExecutionTarget.INSTANCE,
+                            configuration,
+                            null)
         }
+    }
+
+    private fun getConfigurationName(buildInvocation: BuildInvocation): String {
+        return if (buildInvocation.skipDependencies) {
+            "${buildInvocation.target} (without dependencies)"
+        } else {
+            buildInvocation.target
+        }
+    }
+
+    private fun getExecutor(buildInvocation: BuildInvocation): Executor {
+        return if (buildInvocation.debugMode)
+            DefaultDebugExecutor.getDebugExecutorInstance() else DefaultRunExecutor.getRunExecutorInstance()
+    }
+
+    private fun createConfigurationAndSettings(buildInvocation: BuildInvocation): RunnerAndConfigurationSettings {
+        val configurationType = runManager.configurationFactories.single { it -> it is DotNetProjectConfigurationType }
+        val configurationFactory = configurationType.configurationFactories.first()
+        val configuration = runManager
+                .createRunConfiguration(getConfigurationName(buildInvocation), configurationFactory)
+
+        configuration.isTemporary = true
+
+        val buildProjectFile = FileUtil.toSystemIndependentName(buildInvocation.projectFile)
+        val dotNetProject = project.solution.runnableProjectsModel.projects.valueOrNull!!
+                .single { it -> it.projectFilePath == buildProjectFile }
+
+        val dotNetConfiguration = configuration.configuration as DotNetProjectConfiguration
+        dotNetConfiguration.parameters.projectFilePath = dotNetProject.projectFilePath
+        dotNetConfiguration.parameters.projectKind = dotNetProject.kind
+        dotNetConfiguration.parameters.programParameters = buildInvocation.target
+        if (buildInvocation.skipDependencies)
+            dotNetConfiguration.parameters.programParameters += " -skip"
+
+        configuration.checkSettings()
+
+        runManager.addConfiguration(configuration)
+        return configuration
     }
 }
